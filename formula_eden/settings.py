e937b80ad358211cd9bd82251fdf9618c08ba5bb
0Nikola0 <dev0nikola0@gmail.com>
2022-07-23 13:52:10 +0200

    fix: merge fixes

--- formula_eden/settings.py
+++ formula_eden/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -136,3 +137,6 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL='/login/'
+
--- formula_eden/urls.py
+++ formula_eden/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('formula_app.urls')),
+    path('', include('formula_app.urls', namespace='formula_app')),
 ]
