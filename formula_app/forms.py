bf091e82b346f110033f2299b0b2db222daa68bd
0Nikola0 <dev0nikola0@gmail.com>
2022-07-26 20:24:04 +0200

    updated form fields

--- formula_app/forms.py
+++ formula_app/forms.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class NewUserForm(UserCreationForm):
 	class Meta:
 		model = User
-		fields = ("username", "email", "password1", "password2")
+		fields = ("username", "password1")
 
 	def save(self, commit=True):
 		user = super(NewUserForm, self).save(commit=False)
