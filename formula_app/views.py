52681f9a5496b04d0766d497a5f691d36eebc3ef
0Nikola0 <dev0nikola0@gmail.com>
2022-07-26 20:29:08 +0200

    changed everything from json reading to database reading

--- formula_app/views.py
+++ formula_app/views.py
@@ -1,141 +1,97 @@
-import json
-from threading import Thread
+import datetime
+from django.utils import timezone
 import traceback
 from django.shortcuts import render, redirect
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
 from django.contrib.auth import login
 from django.contrib import messages
-from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import user_passes_test
 from django.db.models import Q
-from requests import request
-from formula_app.forms import NewUserForm
 from formula_app.scrapers.standings import get_driver_standings, get_constructor_standings, get_races
 from formula_app.scrapers import telma, sportskimk, sporteden
 from .models import Tim, Vest, Vozac, Trka
+from formula_app.forms import NewUserForm
 
-"""
-    dataJson so go ima u sekoj context dict 
-    e za js skriptata so meste u navbar koja trka e sledna
-    TODO: ako e sredeno countdown.js da rabote od static proveri dali moze da se poednostave tuka
-    TODO: toa dataJSON treba da se naprae da zima od database a ne od JSON so ima zacuvuvano od porano
-"""
 
 def home(request):
-    data = {}
-    with open("formula_app/data/trki/trki.json", 'r') as read_file:
-        data = json.load(read_file)
-    
-    dataJSON = json.dumps(data)
+    sledna_trka = Trka.objects.filter(pocetok__gte=datetime.datetime.now()).order_by("pocetok").first()
+    sledna_sesija = sledna_trka.sesii.all().filter(datum__gte=timezone.now()).order_by("datum").first()
     context = {
-        "data": dataJSON
+        "sledna_trka": sledna_trka,
+        "sledna_sesija": sledna_sesija,
     }
 
     return render(request, 'formula_app/odbrojuvanje.html', context)
 
 
 def novosti(request):
-    data = {}
-    with open("formula_app/data/trki/trki.json", 'r') as read_file:
-        data = json.load(read_file)
-    
-    dataJSON = json.dumps(data)
-
     # "-skrejp_datum" e rastecki a samo "skrejpy_datum" e opagjacki
     # ili obratno proveri koa ke scrape nes nova vest
     vesti = Vest.objects.all().order_by("-skrejp_datum")
+    sledna_trka = Trka.objects.filter(pocetok__gte=datetime.datetime.now()).order_by("pocetok").first()
     
     context = {
         "veesti": vesti,
-        "data": dataJSON,
+        "sledna_trka": sledna_trka,
     }
 
     return render(request, 'formula_app/novosti.html', context)
 
 
 def plasman(request):
-    data = {}
-    with open("formula_app/data/trki/trki.json", 'r') as read_file:
-        data = json.load(read_file)
-    
-    dataJSON = json.dumps(data)
+    sledna_trka = Trka.objects.filter(pocetok__gte=datetime.datetime.now()).order_by("pocetok").first()
     vozaci = Vozac.objects.all()
     timovi = Tim.objects.all().filter(~Q(ime="default"))
 
     context = {
         "vozaci": vozaci,
         "timovi": timovi,
-        "data": dataJSON,
+        "sledna_trka": sledna_trka,
     }
 
     return render(request, 'formula_app/plasman.html', context)
 
 
 def raspored(request):
-    data = {}
-    with open("formula_app/data/trki/trki.json", 'r') as read_file:
-        data = json.load(read_file)
-    
-    dataJSON = json.dumps(data)
     trki = Trka.objects.all()
-    # print(trki.sesii)
+    sledna_trka = Trka.objects.filter(pocetok__gte=datetime.datetime.now()).order_by("pocetok").first()
+
     context = {
         "trki": trki,
-        "data": dataJSON,
+        "sledna_trka": sledna_trka,
     }
 
     return render(request, 'formula_app/raspored.html', context)
 
 
-def traka_info(request, traka_id):
-    if request.method == "GET":
-        trki = []
-        with open("formula_app/data/trki/trki.json", 'r') as read_file:
-            trki = json.load(read_file)["results"]
-        
-    data = {}
-    with open("formula_app/data/trki/trki.json", 'r') as read_file:
-        data = json.load(read_file)
-    
-    dataJSON = json.dumps(data)
-        
-    # TODO
-    # tuka nekakov exception treba za ako ne ja najde taa trka u json
-    # so ne treba da se sluce nikogas zatoa so od isti json zimat datata
-    # ama za sekoj slucaj
-    context = {}
-    for i in trki:
-        if int(i['race_id']) == int(traka_id):
-            context = {
-                "traka": i,
-                "data": dataJSON,
-            }
+def traka_info(request, traka_id): 
+    trki = Trka.objects.all()
+    selektirana_trka = trki.filter(race_id=traka_id).first()
+    sledna_trka = trki.filter(pocetok__gte=datetime.datetime.now()).order_by("pocetok").first()
+
+    context = {
+        "sledna_trka": sledna_trka,
+        "traka": selektirana_trka,
+    }
 
     return render(request, 'formula_app/traka-info.html', context)
 
 
-
 def otvorena_novost(request, novost_id):
-    data = {}
-    with open("formula_app/data/trki/trki.json", 'r') as read_file:
-        data = json.load(read_file)
-    
-    dataJSON = json.dumps(data)
+    sledna_trka = Trka.objects.filter(pocetok__gte=datetime.datetime.now()).order_by("pocetok").first()
     selektirana_vest = Vest.objects.get(custom_id=novost_id)
+    vesti = Vest.objects.all()[:2][::-1]
 
     context = {
+        "sledna_trka": sledna_trka,
         "novost": selektirana_vest,
-        "data": dataJSON,
+        "vesti": vesti,
     }
 
     return render(request, 'formula_app/otvorena-vest.html', context)
-
-
-@login_required()
-def gledaj(request):
-    return render(request, 'formula_app/strimanje.html')
 
 
 @staff_member_required()
@@ -212,40 +168,59 @@ def manage(request):
             context = {
                 "status_message": tb,
             }
+    sledna_trka = Trka.objects.filter(pocetok__gte=datetime.datetime.now()).order_by("pocetok").first()
+    context["sledna_trka"] = sledna_trka
 
     return render(request, 'formula_app/manage.html', context=context)
 
 
+# Treba REDIRECT da se srede za da rabote ovoa dobro so u.is_auth
+# @user_passes_test(lambda u: not u.is_authenticated)
 def register_request(request):
-	if request.method == "POST":
-		form = NewUserForm(request.POST)
-		if form.is_valid():
-			user = form.save()
-			login(request, user)
-			messages.success(request, "Registration successful." )
-			return redirect("formula_app:app-welcome")
-		messages.error(request, "Unsuccessful registration. Invalid information.")
-	form = NewUserForm()
-	return render (request=request, template_name="formula_app/register.html", context={"register_form":form})
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful." )
+            return redirect("formula_app:app-welcome")
+        messages.error(request, "Unsuccessful registration. Invalid information.")
+    form = NewUserForm()
+
+    sledna_trka = Trka.objects.filter(pocetok__gte=datetime.datetime.now()).order_by("pocetok").first()
+    context = {
+        "register_form":form,
+        "sledna_trka": sledna_trka,
+    }
+
+    return render (request=request, template_name="formula_app/register.html", context=context)
 
 
+# @user_passes_test(lambda u: not u.is_authenticated)
 def login_request(request):
-	if request.method == "POST":
-		form = AuthenticationForm(request, data=request.POST)
-		if form.is_valid():
-			username = form.cleaned_data.get('username')
-			password = form.cleaned_data.get('password')
-			user = authenticate(username=username, password=password)
-			if user is not None:
-				login(request, user)
-				messages.info(request, f"You are now logged in as {username}.")
-				return redirect("formula_app:app-welcome")
-			else:
-				messages.error(request,"Invalid username or password.")
-		else:
-			messages.error(request,"Invalid username or password.")
-	form = AuthenticationForm()
-	return render(request=request, template_name="formula_app/login.html", context={"login_form":form})
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect("formula_app:app-welcome")
+            else:
+                messages.error(request,"Invalid username or password.")
+        else:
+            messages.error(request,"Invalid username or password.")
+    form = AuthenticationForm()
+
+    sledna_trka = Trka.objects.filter(pocetok__gte=datetime.datetime.now()).order_by("pocetok").first()
+    context = {
+        "login_form":form,
+        "sledna_trka": sledna_trka,
+    }
+
+    return render(request=request, template_name="formula_app/login.html", context=context)
 
 
 def welcome(request):
