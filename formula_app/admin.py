684ea0d40e366a88c09ab6974b77af869e6f130c
0Nikola0 <dev0nikola0@gmail.com>
2022-07-26 20:18:28 +0200

    added admin dispaly for models

--- formula_app/admin.py
+++ formula_app/admin.py
@@ -1,8 +1,37 @@
 from django.contrib import admin
-from .models import Tim, Vozac, Sesija, Trka, Vest
+from .models import Tim, Vozac, Sesija, Trka, Vest, TrkaSesija
 
-admin.site.register(Tim)
-admin.site.register(Vozac)
-admin.site.register(Sesija)
-admin.site.register(Trka)
-admin.site.register(Vest)
+
+class TimAdmin(admin.ModelAdmin):
+    list_display = ('ime',)
+
+    
+class VozacAdmin(admin.ModelAdmin):
+    list_display = ('ime', 'prezime', 'tim')
+
+    
+class SesijaAdmin(admin.ModelAdmin):
+    readonly_fields = ('session_id',)
+    list_display = ('session_id', 'ime', 'trka_ime', 'datum')
+
+    
+class TrkaAdmin(admin.ModelAdmin):
+    readonly_fields = ('race_id',)
+    list_display = ('ime', 'pocetok', 'status')
+
+    
+class VestAdmin(admin.ModelAdmin):
+    readonly_fields = ('custom_id',)
+    list_display = ('naslov', 'skrejp_datum')
+
+
+class TrkaSesijaAdmin(admin.ModelAdmin):
+    list_display = ('trka', 'sesija')
+
+
+admin.site.register(Tim, TimAdmin)
+admin.site.register(Vozac, VozacAdmin)
+admin.site.register(Sesija, SesijaAdmin)
+admin.site.register(Trka, TrkaAdmin)
+admin.site.register(Vest, VestAdmin)
+admin.site.register(TrkaSesija, TrkaSesijaAdmin)
