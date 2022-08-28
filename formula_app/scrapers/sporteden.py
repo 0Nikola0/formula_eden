7abec522245a7e19aeeb74914e949d34f9ed4d3e
0Nikola0 <dev0nikola0@gmail.com>
2022-08-28 19:03:43 +0200

    fix: hronoloski redosled

--- formula_app/scrapers/sporteden.py
+++ formula_app/scrapers/sporteden.py
@@ -7,6 +7,7 @@ from ..models import Vest
 
 
 def scrape() -> str:
+    vesti = []
     message = "Vesti: {\n"
 
     SITE_URL = "https://sport1.mk/koli-motori-i-trki"
@@ -41,16 +42,26 @@ def scrape() -> str:
         except AttributeError:
             continue
 
+        vesti.append({
+            "v_naslov": v_naslov,
+            "v_privju": v_privju,
+            "v_url": v_url,
+            "v_slika_url": v_slika_url,
+            "v_teksto": v_teksto,
+        })
+
+    # Scrape prae od najnova do najstara i ako ne napraam reverse [::-1] na stranata ne gi pokazuva po ubav redosled
+    for v in vesti[::-1]:
         try:
-            vest = get_object_or_404(Vest, naslov=v_naslov)
+            vest = get_object_or_404(Vest, naslov=v["v_naslov"])
         except Http404:
-            vest = Vest.objects.create(naslov=v_naslov)
-            vest.privju = v_privju
-            vest.url = v_url
-            vest.slika = v_slika_url
-            vest.tekst = v_teksto
+            vest = Vest.objects.create(naslov=v["v_naslov"])
+            vest.privju = v["v_privju"]
+            vest.url = v["v_url"]
+            vest.slika = v["v_slika_url"]
+            vest.tekst = v["v_teksto"]
             vest.source = "sport1.mk"
-            message += f"\t\"{v_naslov}\",\n"
+            message += f"\t\"{v['v_naslov']}\",\n"
 
         vest.save()
 
--- formula_app/scrapers/telma.py
+++ formula_app/scrapers/telma.py
@@ -7,6 +7,7 @@ from ..models import Vest
 
 
 def scrape() -> str:
+    vesti = []
     message = "Vesti: {\n"
     
     SITE_URL = "https://telma.com.mk/kategorija/%D1%81%D0%BF%D0%BE%D1%80%D1%82/%D1%84%D0%BE%D1%80%D0%BC%D1%83%D0%BB%D0%B0-1/"
@@ -37,16 +38,26 @@ def scrape() -> str:
         except AttributeError:
             continue
 
+        vesti.append({
+            "v_naslov": v_naslov,
+            "v_privju": v_privju,
+            "v_url": v_url,
+            "v_slika_url": v_slika_url,
+            "v_teksto": v_teksto,
+        })
+
+    # Scrape prae od najnova do najstara i ako ne napraam reverse [::-1] na stranata ne gi pokazuva po ubav redosled
+    for v in vesti[::-1]:
         try:
-            vest = get_object_or_404(Vest, naslov=v_naslov)
+            vest = get_object_or_404(Vest, naslov=v["v_naslov"])
         except Http404:
-            vest = Vest.objects.create(naslov=v_naslov)
-            vest.privju = v_privju
-            vest.url = v_url
-            vest.slika = v_slika_url
-            vest.tekst = v_teksto
+            vest = Vest.objects.create(naslov=v["v_naslov"])
+            vest.privju = v["v_privju"]
+            vest.url = v["v_url"]
+            vest.slika = v["v_slika_url"]
+            vest.tekst = v["v_teksto"]
             vest.source = "telma.mk"
-            message += f"\t\"{v_naslov}\"\n"
+            message += f"\t\"{v['v_naslov']}\",\n"
 
         vest.save()
     
